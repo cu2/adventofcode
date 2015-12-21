ea6161fd94fbd433ebc9dd6f8bd38a63e1b45bdd
Andras Nemeth <cucu.szpem@gmail.com>
2015-12-21 21:17:50 +0100

    [4] input moved to file

--- 4/solution.py
+++ 4/solution.py
@@ -1,6 +1,9 @@
 import hashlib
+import os
+input_filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'input')
 
-secret_key = 'iwrupvqb'
+with open(input_filename, 'r') as f:
+    secret_key = f.read()
 
 answer = 1
 while hashlib.md5(secret_key + str(answer)).hexdigest()[:5] != '00000':
